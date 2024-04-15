3b207dd94d7f15fe584c4a5c4b43f2b7a0330c2b
NicuBac <baciu.nicolae1998@gmail.com>
2024-04-15 23:17:16 +0300

    Homework 2 feedback

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import styles from './Feedback.module.css';
+import PropTypes from 'prop-types';
+
+class FeedbackOptions extends Component {
+    render() {
+        const { options, onLeaveFeedback } = this.props;
+    return (
+    <div>
+    {options.map(option => (
+      <button className= {styles.options} key={option} onClick={() => onLeaveFeedback(option)}>
+        {option}
+      </button>
+    ))}
+  </div>
+);
+    }
+}
+FeedbackOptions.propTypes = {
+    options: PropTypes.array.isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired
+}
+export default FeedbackOptions;
